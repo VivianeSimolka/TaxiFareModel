52dc0df10d4c32b8c52d8be0c8e737d119255705
Viviane Simolka <viviane.thomas@gmail.com>
2022-02-22 15:19:59 +0100

    1st loop implementation for model selection

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,20 +1,47 @@
-from matplotlib.pyplot import get
+#from matplotlib.pyplot import get
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, SGDRegressor, Lasso, PoissonRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-#from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import train_test_split
+import joblib
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
-#from TaxiFareModel.data import get_data, clean_data
+from TaxiFareModel.data import get_data, clean_data
+
+MODELS = {"linear_model": LinearRegression(),
+                        "ridge_model": Ridge(),
+                        "lasso_model": Lasso(),
+                        "sgd_model": SGDRegressor(),
+                       # "poisson_model": PoissonRegressor(),
+                        "svm_model": SVR(),
+                        "tree_model": DecisionTreeRegressor(),
+                        "forest_model": RandomForestRegressor()}
 
 class Trainer:
 
-    def __init__(self, X_train, y_train):
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[DE] [Berlin] [VivianeSimolka] Taxi fare challenge"
+    MODELS = {"linear_model": LinearRegression(),
+                        "ridge_model": Ridge(),
+                        "lasso_model": Lasso(),
+                        "sgd_model": SGDRegressor(),
+                        #"poisson_model": PoissonRegressor(),
+                        "svm_model": SVR(),
+                        "tree_model": DecisionTreeRegressor(),
+                        "forest_model": RandomForestRegressor()}
+
+    def __init__(self, X_train, y_train, model="linear_model"):
         self.X_train = X_train
         self.y_train = y_train
-
+        self.model_name = model
+        self.model = self.MODELS[model]
 
     def set_pipeline(self):
      '''returns a pipelined model'''
@@ -32,7 +59,7 @@ class Trainer:
         ], remainder="drop")
      self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('linear_model', self.model)
         ])
      return self.pipeline
 
@@ -48,6 +75,33 @@ class Trainer:
         rmse = compute_rmse(y_pred, y_test)
         return rmse
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, f'{self.model_name}.joblib')
+
+
 
 #    def get_clean_data(self, nrows=10000):
 #        data = get_data(nrows)
@@ -59,3 +113,20 @@ class Trainer:
 #        X = self.data.drop(columns="fare_amount")
 #        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size)
 #        return self.X_train, self.X_test, self.y_train, self.y_test
+
+if __name__ == '__main__':
+    #model = "tree_model"
+    df = get_data()
+    data = clean_data(df)
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    for model in MODELS.keys():
+        #import ipdb; ipdb.set_trace
+        trainer = Trainer(X_train, y_train, model=model)
+        trainer.run()
+        rmse = trainer.evaluate(X_test, y_test)
+        #trainer.mlflow_run()
+        trainer.mlflow_log_param("model", trainer.model_name)
+        trainer.mlflow_log_metric("rmse", rmse)
